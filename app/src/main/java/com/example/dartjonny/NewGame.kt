37aa3692d712509229d83312af71d7b87663cf91
Calle <skoglund08@hotmail.com>
2023-02-24 20:50:03 +0100

    First commit

---
+++ app/src/main/java/com/example/dartjonny/NewGame.kt
@@ -0,0 +1,12 @@
+package com.example.dartjonny
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+class NewGame : ComponentActivity() {
+    @Composable
+    fun addPlayer() {
+
+    }
+}
