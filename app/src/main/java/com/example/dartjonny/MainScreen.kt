8c048757dc1998ace6d7d8c7d3378acec14974c9
Calle <skoglund08@hotmail.com>
2023-03-17 17:11:28 +0100

    app background

--- app/src/main/java/com/example/dartjonny/MainScreen.kt
+++ app/src/main/java/com/example/dartjonny/MainScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -20,71 +21,74 @@ import androidx.navigation.NavController
 
 @Composable
 fun MainScreen(navController: NavController) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center,
-    ) {
-        Text(
-            text = "DartJonny",
-            fontSize = 50.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center
-        )
-    }
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Card(
-            modifier = Modifier
-                .padding(20.dp),
-            backgroundColor = Color(139, 137, 137, 255),
-            elevation = 2.dp
+    Column(modifier = Modifier.fillMaxSize().paint(painterResource(id = R.drawable.app_background),contentScale = ContentScale.FillWidth)) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
         ) {
-            Column() {
-                Row(
-                    modifier = Modifier
-                        .padding(all = 10.dp)
-                        .height(60.dp)
-                        .fillMaxWidth()
-                        .background(Color(255, 85, 0))
-                        .clickable(onClick = { navController.navigate(Screen.NewGameScreen.route) }),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    Text(text = "Nytt spel", modifier = Modifier.padding(start = 20.dp))
-                    Spacer(modifier = Modifier.width(width = 90.dp))
-                    Image(
-                        painter = painterResource(id = R.drawable.minidart),
-                        contentDescription = "",
-                        contentScale = ContentScale.Fit
-                    )
-                }
+            Text(
+                text = "DartJonny",
+                fontSize = 50.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
 
-                Spacer(modifier = Modifier.height(height = 20.dp))
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Card(
+                modifier = Modifier
+                    .padding(20.dp),
+                backgroundColor = Color(139, 137, 137, 255),
+                elevation = 2.dp
+            ) {
+                Column() {
+                    Row(
+                        modifier = Modifier
+                            .padding(all = 10.dp)
+                            .height(60.dp)
+                            .fillMaxWidth()
+                            .background(Color(255, 85, 0))
+                            .clickable(onClick = { navController.navigate(Screen.NewGameScreen.route) }),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        Text(text = "Nytt spel", modifier = Modifier.padding(start = 20.dp))
+                        Spacer(modifier = Modifier.width(width = 90.dp))
+                        Image(
+                            painter = painterResource(id = R.drawable.minidart),
+                            contentDescription = "",
+                            contentScale = ContentScale.Fit
+                        )
+                    }
 
-                Row(
-                    modifier = Modifier
-                        .padding(all = 10.dp)
-                        .height(60.dp)
-                        .fillMaxWidth()
-                        .background(Color.White)
-                        .clickable(onClick = { navController.navigate(Screen.LeaderboardScreen.route) }),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    Text(
-                        text = "Leaderboard",
-                        modifier = Modifier.padding(start = 20.dp),
-                    )
-                    Spacer(modifier = Modifier.width(width = 65.dp))
-                    Image(
-                        painter = painterResource(id = R.drawable.leaderboard),
-                        contentDescription = "",
-                        contentScale = ContentScale.Fit
-                    )
+                    Spacer(modifier = Modifier.height(height = 20.dp))
+
+                    Row(
+                        modifier = Modifier
+                            .padding(all = 10.dp)
+                            .height(60.dp)
+                            .fillMaxWidth()
+                            .background(Color.White)
+                            .clickable(onClick = { navController.navigate(Screen.LeaderboardScreen.route) }),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            text = "Leaderboard",
+                            modifier = Modifier.padding(start = 20.dp),
+                        )
+                        Spacer(modifier = Modifier.width(width = 65.dp))
+                        Image(
+                            painter = painterResource(id = R.drawable.leaderboard),
+                            contentDescription = "",
+                            contentScale = ContentScale.Fit
+                        )
+                    }
                 }
             }
         }
